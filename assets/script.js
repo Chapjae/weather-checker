5506727d124a56f55aa6022cf5713abb43681c1f
Jay Liu <jayliu89@gmail.com>
2023-07-11 00:01:43 -0700

    added api calls for 5 day forecast and api call for main city info. working on jumbotron display

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,45 @@
-import key from "key.js"
 
-function getGeocode() {
-    var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&APPID=${key}`
+function getCityData(city) {
+    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=72848a6e5462cfcb20726fb97310cab3&units=metric`
 
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            data[0].lat
-            data[0].lon
+            console.log(data)
+            var lat = data.coord.lat
+            var lon = data.coord.lon
+            
+            $("#city-name").text(data.name) + dayjs().format("MMM-dddd-YYYY")
+            $("#wind").text(data.wind.speed)
+            $("#humidity").text(data.main.humidity)
+            
+            getFiveDayForecast(lat, lon)
+        })
+        .catch(function (err) {
         })
 }
 
-function getCity(lat, lon) {
-    var requestUrl = `https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=${lat}lon=${lon}&dt=1643803200&appid=${key}`
+$(".btn").on("click", function() {
+    var city = $(".search-by-city").val()
+    var citiesLi = $("<li></li>").text(city);
+    
+    citiesLi.addClass("city-group")
+    $("#searched-cities").append(citiesLi)
+
+    getCityData(city)
+})
+// `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=`
+
+function getFiveDayForecast(lat, lon){
+    var requestUrl = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=5&appid=72848a6e5462cfcb20726fb97310cab3&units=metric`
+    
+    fetch(requestUrl)
+        .then(function (response) {
+            console.log(response.json());
+        })
+        .then(function (data) {
+
+        })
 }
