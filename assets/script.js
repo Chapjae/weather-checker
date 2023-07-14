df38e2fde2553d75cacd93e4addbc7492403eb14
Jay Liu <jayliu89@gmail.com>
2023-07-13 20:22:45 -0700

    added comments to js file

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,8 @@ var loadSearchedCities = JSON.parse(localStorage.getItem("searchedCities"))
 var cityUl = document.getElementById("searched-cities")
 var clearhistorybtn = document.getElementById("clearsearch")
 
-if(loadSearchedCities){
+// if local storage is found, create li's out of text and append them to cityUL 
+if (loadSearchedCities) {
     loadSearchedCities.forEach(function(loadSearchedCities) {
         var citiesLi = document.createElement("li");
         citiesLi.classList.add("city-group", "list-group-item", "list-group-item-action",  "active")
@@ -15,7 +16,6 @@ if(loadSearchedCities){
     })
 }
 
-
 function getCityData(city) {
     var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=72848a6e5462cfcb20726fb97310cab3&units=imperial`
 
@@ -23,11 +23,12 @@ function getCityData(city) {
         .then(function (response) {
             return response.json();
         })
+        // obtain lat and lon coordinates to pass to 5 day forecast
         .then(function (data) {
             console.log(data)
             var lat = data.coord.lat
             var lon = data.coord.lon
-            
+        // if data code returns 200, create variables and set text content of various elements to display properites from fetch call
         if (data.cod == 200) {
             var citySearch = document.getElementById("searchInput").value;
             var citiesLi = document.createElement("li");
@@ -43,6 +44,7 @@ function getCityData(city) {
             citiesLi.classList.add("city-group", "list-group-item", "list-group-item-action",  "active")
             citiesLi.textContent = citySearch
             
+            // if a user searches for a city that is already on the cityUl, it won't be appended
             var existingLi = Array.from(cityUl.getElementsByTagName("li")).find(function(li) {
                 return li.textContent === citySearch;
             });
@@ -51,6 +53,7 @@ function getCityData(city) {
                 cityUl.appendChild(citiesLi)
             }
             
+            // fill out the jumobtron with city information
             city.textContent = "City: " + data.name + " " + currentDay + "   ";
             city.appendChild(icons)
             
@@ -58,20 +61,24 @@ function getCityData(city) {
             wind.textContent ="Wind speed: " + data.wind.speed
             humid.textContent ="Humidity: " + data.main.humidity
             
+            // add an eventlistener to each li to recall previously searched data
             citiesLi.addEventListener("click", function() {
                 getCityData(this.textContent);
             })
             
+            // create an array of all li elements on on cityUl
             var liElements = Array.from(cityUl.getElementsByTagName("li"));
             var cityTexts = liElements.map(function(li) {
               return li.textContent;
             });
 
+            // set cityTexts into localstorage
             localStorage.setItem("searchedCities", JSON.stringify(cityTexts));
         }
-
-        getFiveDayForecast(lat, lon)
+            // call five day forecast
+            getFiveDayForecast(lat, lon)
         })
+        // if the user enters a city that doesn't exist, return an alert
         .catch(function (err) {
             if (err) {
                 alert("City not Found!")
@@ -90,6 +97,7 @@ document.getElementById("clearsearch").addEventListener("click", function() {
     localStorage.clear();
 });
 
+// fill out the 5 other weather day cards with appropriate information
 function getFiveDayForecast(lat, lon){
     var requestUrl = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=5&appid=72848a6e5462cfcb20726fb97310cab3&units=imperial`
     
